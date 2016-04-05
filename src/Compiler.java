0e9f3e40a21820f4e31de233d62bfabc59d82cac
Charles <raverblazer@gmail.com>
2016-04-05 13:09:12 -0300

    Add all statements, and fix a couple things.

--- src/Compiler.java
+++ src/Compiler.java
@@ -17,7 +17,7 @@ public class Compiler {
 
     return new Program(declaration());
   }
-    
+
   public StatementBlock declaration(){
     StatementBlock stmtBlock = null;
     if(token == 'v') {
@@ -29,6 +29,9 @@ public class Compiler {
           if(token == ')') {
             nextToken();
             stmtBlock = stmtBlock();
+
+            ArrayList<Variable> variables_array = new ArrayList<Variable>();
+            variables_array = variableDeclaration();
           }
           else
             error();
@@ -44,30 +47,30 @@ public class Compiler {
 
     return stmtBlock;
   }
-  
+
   public void variableDeclaration() {
-    ArrayList<Variable> variables = new ArrayList<>();
-    
-    while(token != 'i' || token != 'd' || token != 'c'){
+    ArrayList<Variable> variables = new ArrayList<Variable>();
+
+    while(token == 'i' || token == 'd' || token == 'c'){
       variables.add(variable());
     }
   }
-  
-  public Variable variable() {  
-    type();
+
+  public Variable variable() {
+    char var_type = type();
     Identifier ident = identifier();
-    
+
     nextToken();
     if(token != ';')
       error();
-    
-    return new Variable(varType, ident);
+
+    nextToken();
+    return new Variable(var_type, ident);
   }
-  
+
   public void type() {
-    if(token == 'i' || token == 'c' || token == 'd')
-      nextToken();
-    
+    char var_type = token;
+    nextToken();
     if(token == '['){
       nextToken();
       if(token == ']')
@@ -75,28 +78,32 @@ public class Compiler {
       else
         error();
     }
+    return var_type;
   }
-  
+
   public void standardType() {
-    
+
   }
-  
+
   public void arrayType() {
-    
+
   }
-  
+
   public StatementBlock stmtBlock() {
     if(token == '{') {
       nextToken();
       variableDeclaration();
       statement();
+      if(token != '}'){
+        error();
+      }
     }
     else
       error();
-    
+
     return new StatementBlock();
   }
-  
+
   public void statement() {
     switch(token) {
       case 'f':
@@ -109,73 +116,154 @@ public class Compiler {
         printStatement();
       default:
         expression();
+        nextToken();
+        if(token != ';'){
+          error();
+        }
     }
   }
-  
+
   public void ifStatement() {
-    
+    nextToken();
+    if(token == '('){
+      nextToken();
+      expression();
+      nextToken();
+      if(token == '{'){
+        nextToken();
+        // How to detect expression inside statement here?
+        // If it detects any letter besides the reserved ones should it loop?
+        while(token == 'f' || token == 'w' || token == 'b' || token == 'p'){
+          statement();
+        }
+        nextToken();
+        if(token != '}'){
+          error();
+        }
+        nextToken();
+        if(token == 'e'){
+          nextToken();
+          if(token == '{'){
+            nextToken();
+            while(token == 'f' || token == 'w' || token == 'b' || token == 'p'){
+              statement();
+            }
+            nextToken();
+            if(token != '}'){
+              error();
+            }
+          } else{
+            error();
+          }
+        }
+      } else{
+        error();
+      }
+    } else{
+      error();
+    }
   }
-  
+
   public void whileStatement() {
-    
+    nextToken();
+    if(token == '('){
+      nextToken();
+      expression();
+      nextToken();
+      if(token == ')'){
+        nextToken();
+        if(token == '{'){
+          nextToken();
+          while(token == 'f' || token == 'w' || token == 'b' || token == 'p'){
+            statement();
+          }
+          nextToken();
+          if(token != '}'){
+            error();
+          }
+        } else{
+          error();
+        }
+      } else{
+        error();
+      }
+    } else{
+      error();
+    }
   }
-  
+
   public void breakStatement() {
-    
+    nextToken();
+    if(token != ';')
+      error();
   }
-  
+
   public void printStatement() {
-    
+    nextToken();
+    if(token == '('){
+      expression();
+      nextToken();
+      while(token == ','){
+        expression();
+        nextToken();
+      }
+      nextToken();
+      if(token != ')'){
+        error();
+      }
+    } else{
+      error();
+    }
   }
-  
+
   public void expression() {
-    
+
   }
-  
+
   public void simpleExpression() {
-    
+
   }
-  
+
   public void term() {
-    
+
   }
-  
+
   public void factor() {
-    
+
   }
-  
+
   public void leftValue() {
-    
+
   }
-  
+
   public void identifier() {
-    
+
   }
-  
+
   public void relationalOperator() {
-    
+
   }
-  
+
   public void addOperator() {
-    
+
   }
-  
+
   public void multiplicationOperator() {
-    
+
   }
-  
+
   public void unary() {
-    
+
   }
-  
+
   public void digit() {
-    
+
   }
-  
+
   public void letter() {
-    
+
   }
-    
+
   public void nextToken() {
     while(tokenPos < input.length && input[tokenPos] == ' ')
       tokenPos++;
@@ -188,11 +276,11 @@ public class Compiler {
     }
     System.out.print(" " + token + " ");
   }
-    
+
   public void error() {
-    if ( tokenPos == 0 ) 
-     tokenPos = 1; 
-    else 
+    if ( tokenPos == 0 )
+     tokenPos = 1;
+    else
      if ( tokenPos >= input.length )
       tokenPos = input.length;
 
