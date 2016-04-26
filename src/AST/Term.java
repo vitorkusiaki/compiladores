3da9f6ad305d35ec9a0e212ca06fe2e011bca234
vitorkusiaki <vkusiaki@gmail.com>
2016-04-26 15:52:31 -0300

    Fix 'Term' constructor name

--- src/AST/Term.java
+++ src/AST/Term.java
@@ -5,7 +5,7 @@ public class Term {
   final private Factor factor;
   final private ArrayList<MultOperation> operations;
 
-  public ExpressionStatement(Factor factor, ArrayList<MultOperation> operations) {
+  public Term(Factor factor, ArrayList<MultOperation> operations) {
     this.factor = factor
     this.operations = operations;
   }
