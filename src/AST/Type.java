612363b45f9c23a76b2b6bdacbcafe77a379364e
Charles <raverblazer@gmail.com>
2016-04-26 17:33:34 -0300

    Fix Type class and compiler.

--- src/AST/Type.java
+++ src/AST/Type.java
@@ -3,16 +3,25 @@ package AST;
 abstract public class Type {
 
     private String name;
+    private IntNumber length;
 
     public static Type intType = new IntType();
     public static Type doubleType = new DoubleType();
     public static Type charType = new CharType();
 
     public Type(String name) {
-        this.name = name;
+      this.name = name;
+    }
+
+    public void setLength(Integer length) {
+      this.length = length;
     }
 
     public String getName() {
         return name;
+    }
+
+    public IntNumber getLength() {
+      return length;
     }
 }
