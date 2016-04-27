da673c1eb5e133899afeb2d5b3a8ac276f8d456e
Charles <raverblazer@gmail.com>
2016-04-26 22:27:16 -0300

    Main from comp9

--- src/Main.java
+++ src/Main.java
@@ -10,15 +10,19 @@ public class Main {
     Program program;
 
     if(args.length != 2) {
-      System.out.println("Usage:\n java Main input_file output_file");
+      System.out.println("Usage:\n   Main input output");
+      System.out.println("input is the file to be compiled");
+      System.out.println("output is the file where the generated code will be stored");
     } else {
       file = new File(args[0]);
       if(!file.exists() || !file.canRead()) {
+        System.out.println("Either the file " + args[0] + " does not exist or it cannot be read");
         throw new RuntimeException();
       }
       try {
         stream = new FileReader(file);
       } catch(FileNotFoundException e) {
+        System.out.println("Something wrong: file does not exist anymore");
         throw new RuntimeException();
       }
 
@@ -27,34 +31,47 @@ public class Main {
       try {
         numChRead = stream.read(input, 0, (int)file.length());
       } catch(IOException e) {
+        System.out.println("Error reading file " + args[0]);
         throw new RuntimeException();
       }
 
       if(numChRead != file.length()) {
+        System.out.println("Read error");
         throw new RuntimeException();
       }
 
       try {
         stream.close();
       } catch(IOException e) {
+        System.out.println("Error in handling the file " + args[0]);
         throw new RuntimeException();
       }
 
       Compiler compiler = new Compiler();
-      FileOutputStream output;
+      FileOutputStream outputStream;
       try {
-        output = new FileOutputStream(args[1]);
+        outputStream = new FileOutputStream(args[1]);
       } catch(IOException e) {
+        System.out.println("File " + args[1] + " could not be opened for writing");
         throw new RuntimeException();
       }
 
-      PrintWriter pw = new PrintWriter(output);
+      PrintWriter printWriter = new PrintWriter(outputStream);
       program = null;
 
       try {
-        program = compiler.compile(input, pw);
+        program = compiler.compile(input, printWriter);
       } catch(RuntimeException e) {
         System.out.println(e);
+      }
+
+      if(program != null) {
+        PW pw = new PW();
+        pw.set(printWriter);
+        // program.genC(pw);
+        if(printWriter.checkError()) {
+          System.out.println("There was an error in the output");
+        }
       }
     }
   }
